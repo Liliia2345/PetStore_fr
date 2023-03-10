09566165f11e36380ffa0f612219a94af6d66c65
unknown <savithakrishnan2021@gmail.com>
2023-03-10 13:54:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Footer from './components/Footer'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<div>
+    
+      <h1 className ='justify-center text-5xl text-violet-400 bg-gray-800'>Welcome to our Petshop</h1>
+      
+      <Footer/>
+      
+      
+
+
+  </div>
+    
   );
 }
 
