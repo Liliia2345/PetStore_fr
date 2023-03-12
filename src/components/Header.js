49d9ec5d67af1410f8fde9d00b278826b0f4e7cb
Priyanka <priyankadavara@gmail.com>
2023-03-12 14:00:15 +0100

    navbar completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Routes, Route } from "react-router-dom";
-import { Typewriter } from "react-simple-typewriter";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <nav>
+      {/* <nav>
         <NavLink to="/" className="link-item ">
           Home
         </NavLink>
@@ -24,34 +29,131 @@ function Header() {
         <button id="join" type="button">
           Join Us
         </button>
-      </nav>
+      </nav> */}
 
-      <div id="headerBgImg">
-        <div className="main_hero">
-          <h1 className="text-5XL">Hello!</h1>
-          <h1>Welcome to our PetShop</h1>
-          <div className="fs-4 px-3 text-2">
-            <span style={{ color: "white" }}>
-              <Typewriter
-                loop
-                cursor
-                cursorStyle="|"
-                typeSpeed={100}
-                deleteSpeed={70}
-                delaySpeed={1000}
-                words={[
-                  "Happy pets, happy life.",
-                  "Where pets are family.",
-                  "Love is a four-legged word.",
-                ]}
-              />
-            </span>
+      <nav className="bg-gray-800">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16">
+            <div className="flex-shrink-0 flex items-center">
+              <NavLink to="/" className="text-white font-bold">
+                PetShop
+              </NavLink>
+            </div>
+            <div className="hidden md:block">
+              <div className="flex items-center space-x-4 px-5 py-3">
+                <NavLink
+                  to="/"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-5 py-3 rounded-md text-sm font-medium"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/about"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-5 py-3 rounded-md text-sm font-medium"
+                >
+                  About
+                </NavLink>
+                <NavLink
+                  to="/adopt"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-5 py-3 rounded-md text-sm font-medium"
+                >
+                  Adopt
+                </NavLink>
+                <NavLink
+                  to="/donate"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-5 py-3 rounded-md text-sm font-medium"
+                >
+                  Donate
+                </NavLink>
+                <NavLink
+                  to="/contact"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-5 py-3 rounded-md text-sm font-medium"
+                >
+                  Contact
+                </NavLink>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={toggleMenu}
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
-          {/* <div id="image_hero">
-              <img src={Ateev1} alt="Ateev" className="image_hero" />
-            </div> */}
         </div>
-      </div>
+        {isOpen ? (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <NavLink
+                to="/"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/about"
+                className="text-gray-300 hover:bg-gray-700  hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/adopt"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Adopt
+              </NavLink>
+              <NavLink
+                to="/donate"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Donate
+              </NavLink>
+              <NavLink
+                to="/contact"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Contact
+              </NavLink>
+            </div>
+          </div>
+        ) : null}
+      </nav>
     </>
   );
 }
--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Carousel from "./Carousel/Carousel";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function ImageCarousel() {
   const [pets, setPets] = useState([]);
@@ -16,25 +17,58 @@ export default function ImageCarousel() {
   }, []);
 
   return (
-    <div
-      style={{
-        maxWidth: 1200,
-        marginLeft: "auto",
-        marginRight: "auto",
-        marginTop: 64,
-      }}
-    >
-      <Carousel show={4}>
-        {pets.map((e) => (
-          <div key={e._id}>
-            <div style={{ padding: 8 }}>
-              {/* <Link to={`/${e._id}`}> */}
-              <img src={e.image} alt="placeholder" style={{ width: "100%" }} />
-              {/* </Link> */}
-            </div>
+    <>
+      <div id="headerBgImg">
+        <div className="main_hero">
+          <h1 className="text-5XL">Hello!</h1>
+          <h1>Welcome to our PetShop</h1>
+          <div className="fs-4 px-3 text-2">
+            <span style={{ color: "orange" }}>
+              <Typewriter
+                loop
+                cursor
+                cursorStyle="|"
+                typeSpeed={100}
+                deleteSpeed={70}
+                delaySpeed={1000}
+                words={[
+                  "Happy pets, happy life.",
+                  "Where pets are family.",
+                  "Love is a four-legged word.",
+                ]}
+              />
+            </span>
           </div>
-        ))}
-      </Carousel>
-    </div>
+          {/* <div id="image_hero">
+              <img src={Ateev1} alt="Ateev" className="image_hero" />
+            </div> */}
+        </div>
+      </div>
+
+      <div
+        style={{
+          maxWidth: 1200,
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: 64,
+        }}
+      >
+        <Carousel show={4}>
+          {pets.map((e) => (
+            <div key={e._id}>
+              <div style={{ padding: 8 }}>
+                {/* <Link to={`/${e._id}`}> */}
+                <img
+                  src={e.image}
+                  alt="placeholder"
+                  style={{ width: "100%" }}
+                />
+                {/* </Link> */}
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
   );
 }
