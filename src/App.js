af6fb1e366f3e00c131a806650b2e19e7c53f112
Priyanka <priyankadavara@gmail.com>
2023-03-12 15:36:24 +0100

    resolved conflict, added Footer, other routes in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
@@ -13,10 +14,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/adopt"></Route>
+          <Route path="/donate"></Route>
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </div>
-        <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink, Routes, Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
